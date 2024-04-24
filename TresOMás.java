5014d5f3f34fff4e62d890877b56dd8d48924fcd
jorgeesg26 <145496908+jorgeesg26@users.noreply.github.com>
2024-04-24 19:49:03 +0200

    Update TresOMás.java

--- TresOMás.java
+++ TresOMás.java
@@ -1,36 +1,115 @@
-import java.util.Scanner;
+public class TresOMás {
 
-public class App {
+    public static void main(String[] args) {
+        System.out.println("TRES O MÁS");
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Introduce un número: ");
-    int numero = sc.nextInt();
-    System.out.print("Simple o en color(s/c) ");
-    String simpleocolor = sc.next();
-    sc.close();
-    if (simpleocolor.equals("s")) {
-      if (numero < 0) {
-        System.out.println("Por favor,introduzca un número positivo");
-      }
-      int invertido = 0;
-      int maxDigito = 0;
-      while (numero > 0) {
-        // multiplicamos el invertido por 10 y le sumamos el resto de la división por 10
-        invertido = invertido * 10 + numero % 10;
-        numero = numero / 10;
-        if (numero % 10 > maxDigito) {
-          //  actualizamos el maxdigito y actualizamos el número dividiéndolo por 10
-          maxDigito = numero % 10;
+        // Bucle para las 10 rondas
+        for (int ronda = 1; ronda <= 10; ronda++) {
+            int puntosJugador1 = 0;
+            int puntosJugador2 = 0;
+
+            // Tiros del jugador 1
+            tirarYSumarPuntos("Jugador 1", ronda, puntosJugador1);
+
+            // Tiros del jugador 2
+            tirarYSumarPuntos("Jugador 2", ronda, puntosJugador2);
+
+            // Mostrar resultados de la ronda
+            System.out.println("Fin de ronda " + ronda + ". Jugador 1 - " + puntosJugador1 +
+                    "; Jugador 2 - " + puntosJugador2);
         }
-        while (invertido > 0) {
-          System.out.println(" ---");
-          System.out.println("| " + invertido % 10 + " |");
-          System.out.println(" ---");
-          // actualizamos el invertido dividiéndolo por 10
-          invertido = invertido / 10;
+
+        // Mostrar ganador final
+        int puntosTotalJugador1 = mostrarGanador(puntosJugador1Total);
+        int puntosTotalJugador2 = mostrarGanador(puntosJugador2Total);
+    }
+
+    private static void tirarYSumarPuntos(String jugador, int ronda, int puntosJugador) {
+        int[] tirada = new int[5];
+        boolean tiradaExtra = false;
+
+        for (int i = 0; i < tirada.length; i++) {
+            tirada[i] = (int) (Math.random() * 6) + 1;
         }
-      }
-    } else if (simpleocolor.equals("c")) {}
-  }
-}
+
+        System.out.print("Tirada " + ronda + " de " + jugador + ": ");
+        for (int i = 0; i < tirada.length; i++) {
+            System.out.print(tirada[i] + (i < tirada.length - 1 ? "-" : ""));
+        }
+
+        puntosJugador += contarPuntosTirada(tirada);
+
+        // Si hay 2 pares, tirada extra
+        if (hayDosPares(tirada)) {
+            tiradaExtra = true;
+            System.out.print("; Tirada Extra");
+
+            while (tiradaExtra) {
+                tirada = new int[5];
+                for (int i = 0; i < tirada.length; i++) {
+                    tirada[i] = (int) (Math.random() * 6) + 1;
+                }
+
+                System.out.print("\nTirada Extra: ");
+                for (int i = 0; i < tirada.length; i++) {
+                    System.out.print(tirada[i] + (i < tirada.length - 1 ? "-" : ""));
+                }
+
+                puntosJugador += contarPuntosTirada(tirada);
+
+                tiradaExtra = hayDosPares(tirada);
+                if (tiradaExtra) {
+                    System.out.print("; Tirada Extra");
+                }
+            }
+        }
+
+        System.out.println("\n" + jugador + " consigue " + puntosJugador + " puntos.");
+    }
+
+    private static int contarPuntosTirada(int[] tirada) {
+        int puntos = 0;
+        int[] contadorNumeros = new int[6];
+
+        for (int numero : tirada) {
+            contadorNumeros[numero - 1]++;
+        }
+
+        for (int i = 0; i < contadorNumeros.length; i++) {
+            if (contadorNumeros[i] == 3) {
+                puntos += 3;
+            } else if (contadorNumeros[i] == 4) {
+                puntos += 6;
+            } else if (contadorNumeros[i] == 5) {
+                puntos += 12;
+            }
+        }
+
+        return puntos;
+    }
+
+    private static boolean hayDosPares(int[] tirada) {
+        int pares = 0;
+        int[] contadorNumeros = new int[6];
+
+        for (int numero : tirada) {
+            contadorNumeros[numero - 1]++;
+        }
+
+        for (int i = 0; i < contadorNumeros.length; i++) {
+            if (contadorNumeros[i] == 2) {
+                pares++;
+            }
+        }
+
+        return pares == 2;
+    }
+
+    private static int mostrarGanador(int puntosJugador) {
+        System.out.println("\n" + puntosJugador + " puntos");
+
+        if (puntosJugador1Total > puntosJugador2Total) {
+            System.out.println("¡Gana el jugador 1!");
+            return puntosJugador1Total;
+        }
+
